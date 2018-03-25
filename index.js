891f9dc30006db8ae823453051c4ca4a7b87b0cf
jpc@wxyz.io Courtney <jpc@wxyz.io>
2018-03-25 01:39:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var name = "Joe"
 var height = "74"
 
-
+ if (name="Susan"){
+   console.log("The name is Correct!")
+ } else {
+   console.log("expected the name to be Susan")
+ }
 // Don't worry about this
 module.exports = { name, height
 }
